a62909934ca7ee5ac499e07a13c76bcf8a68a458
vishalkhopkar <vishalkhopkar20@gmail.com>
2021-05-04 18:06:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import playsound
+import requests
+import datetime
+import json
+import time
+
+def process_output(json_output, region, dist_id):
+    #output = json.loads(json_output)
+    no_of_centres = 0
+    output = json_output
+    centres = output["centers"]
+    for centre in centres:
+        sessions = centre["sessions"]
+        for session in sessions:
+            min_age = session["min_age_limit"]
+            avbl_cap = session["available_capacity"]
+            if min_age == 18 and avbl_cap > 0:
+                print("SLOTS AVAILABLE AT "+centre["name"])
+                no_of_centres+=1
+
+    if no_of_centres == 0:
+        print("Sorry no centre available in at "+region+" dist_id " +str(dist_id)+" as on "+datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S"))
+    else:
+        playsound.playsound("alarm.mp3")
+
+districtCodes = {
+    "Bangalore": [294, 265],
+    "Mumbai (MMR)": [395, 392, 393, 394],
+    "Pune": [363],
+    "Delhi (NCR)": [141, 145, 140, 146, 147, 143, 148, 149, 144, 150, 142, 199, 188, 207, 202, 650, 651],
+    "Hyderabad": [581],
+    "Chennai": [571, 565, 557]
+}
+region_arr = ["Bangalore", "Mumbai MMR", "Pune", "Delhi NCR", "Hyderabad", "Chennai"]
+reg_id = input("Enter city.\n1: Bangalore\n2: Mumbai MMR\n3: Pune\n4: Delhi NCR\n5: Hyderabad\n6: Chennai\n")
+try:
+    reg_id = int(reg_id)
+    if reg_id < 1 or reg_id > 6:
+        raise Exception("Invalid")
+except:
+    print("Invalid input exiting...")
+    exit()
+region = region_arr[reg_id - 1]
+print("Your region is "+region)
+dist_ids = districtCodes[region]
+while 1:
+    current_date = datetime.datetime.now()
+    current_yr = current_date.year
+    current_month = current_date.month
+    current_day = current_date.day
+    str_current_year = str(current_yr)
+    str_current_month = str(current_month)
+    str_current_day = str(current_day)
+    if len(str_current_day) == 1:
+        str_current_day = '0'+str_current_day
+    if len(str_current_month) == 1:
+        str_current_month = '0'+str_current_month
+
+    total_date = str_current_day+"-"+str_current_month+"-"+str_current_year
+    for dist_id in dist_ids:
+
+        response = requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id="+str(dist_id)+"&date="+total_date)
+        if response.status_code == 200:
+            process_output(response.json(), region, dist_id)
+        else:
+            print("API call failed")
+
+    time.sleep(60)
